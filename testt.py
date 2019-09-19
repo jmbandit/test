7ebc83b55b55ef9e7c0f0f51f315d8035c1114ff
jmbandit <jmbarry22@gmail.com>
2019-09-18 19:40:26 -0500

    basic data type examples

--- testt.py
+++ testt.py
@@ -1,3 +1,26 @@
 
 
-phonebook = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
+a = ['spam', 'eggs', 100, 1234] #list
+
+b = ("apple", "banana", "cherry") #tuple
+
+c= {"name" : "John", "age" : 36} #dict
+
+d = {"apple", "banana", "cherry"} #set
+
+e = frozenset({"apple", "banana", "cherry"}) # a frozen set
+
+x = str("Hello World")	                 #str	
+x = int(20)	                              #int	
+x = float(20.5)	                         #float	
+x = complex(1j)	                          #complex	
+x = list(("apple", "banana", "cherry"))	  #list	
+x = tuple(("apple", "banana", "cherry"))	#tuple	
+x = range(6)	                            #range	
+x = dict(name="John", age=36)	             #dict	
+x = set(("apple", "banana", "cherry"))       #	set	
+x = frozenset(("apple", "banana", "cherry"))
+
+print(a)
+print(a[1])
+print(len(a))
